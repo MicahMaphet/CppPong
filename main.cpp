e750782a054b8a846d736b41a54773424ab0109b
MicahMaphet <93409262+MicahMaphet@users.noreply.github.com>
2023-08-14 18:20:17 -0400

    WARNING, this is an unstable version

--- main.cpp
+++ main.cpp
@@ -3,65 +3,42 @@
 #include <math.h>
 #include <string>
 #include <cstdio>
-#include "Moveable.cpp"
+#include "Moveables.cpp"
 
 /**
   * This is the structure of the ball, it has the vector space, x and y
   * speed, and radius.
   */
-struct Ball
+struct Ball : MoveableCircle
 {
 	float x, y;
-	float speedX, speedY;
-	float radius;
+	float xSpeed, ySpeed;
 
-	// Render the circle
-	void Draw()
-	{
-		DrawCircle((int) x, (int) y, radius, WHITE);
-	}
 };
 /**
   * This structure is used for the two paddles, it has the vector space,
-  * height, width, swingVel (which is used for determining how fast the
+  * height, width, xSpeed (which is used for determining how fast the
   * paddle is moving in the x-axis), and xDefault (which is where the paddle
   * oscilates back twoards in the x-axis)
   */
 
-struct Paddle
+struct Paddle : MoveableRect
 {
-	float x, y;
-	float speed;
-	float width, height;
-	float swingVel;
+
 	float xDefault;
-	// Returns, the height, width, x, and y of the rectangle
-	Rectangle GetRect()
-	{
-		return Rectangle{ x - width / 2, y - height / 2,  width, height };
-	}
-	// Draws the rectangle of the paddle
-	void Draw()
-	{
-		DrawRectangleRec(GetRect(), WHITE);
-	}
+	float xSpeed, ySpeed;
 	// Brings the paddle back to the idle state after a swing
 	void BringBack()
 	{
-		swingVel += (xDefault - x) * 0.1;
-		swingVel = swingVel * 0.8;
-		x += swingVel;
+		xSpeed += (xDefault - x) * 0.1;
+		xSpeed = xSpeed * 0.8;
+		x += xSpeed;
 	}
 };
 
 class Particle : public Moveable 
 {
-	Particle(int _x, int _y, int _speed) 
-	{
-		x = _x;
-		y = _y;
-		speed = _speed;
-	}
+	
 };
 
 int main() 
@@ -75,26 +52,26 @@ int main()
 	ball.x = GetScreenWidth() / 2.0f;
 	ball.y = GetScreenHeight() / 2.0f;
 	ball.radius = 5;
-	ball.speedX = 100;
-	ball.speedY = 300;
+	ball.xSpeed = 100;
+	ball.ySpeed = 300;
 	// Initialize the leftPaddle member
 	Paddle leftPaddle;
 	leftPaddle.x = 125;
 	leftPaddle.y = GetScreenHeight() / 2;
 	leftPaddle.width = 15;
 	leftPaddle.height = 100;
-	leftPaddle.speed = 500;
+	leftPaddle.ySpeed = 500;
 	leftPaddle.xDefault = leftPaddle.x;
-	leftPaddle.swingVel = 0;
+	leftPaddle.ySpeed = 0;
 	// Initialize the rightPaddle member
 	Paddle rightPaddle;
 	rightPaddle.x = GetScreenWidth() - 125;
 	rightPaddle.y = GetScreenHeight() / 2;
 	rightPaddle.width = 15;
 	rightPaddle.height = 100;
-	rightPaddle.speed = 500;
+	rightPaddle.ySpeed = 500;
 	rightPaddle.xDefault = rightPaddle.x;
-	rightPaddle.swingVel = 0;
+	rightPaddle.xSpeed = 0;
 	// The most recent swing force on impact of the ball
 	float swingForce = 0;
 
@@ -102,82 +79,82 @@ int main()
 	while (!WindowShouldClose())
 	{
 		// Move the ball by the set speed in x and y, multipied by frame time
-		ball.x += ball.speedX * GetFrameTime();
-		ball.y += ball.speedY * GetFrameTime();
+		ball.x += ball.xSpeed * GetFrameTime();
+		ball.y += ball.ySpeed * GetFrameTime();
 		// Top screen collision
 		if (ball.y < 0)
 		{
 			ball.y = 0;
-			ball.speedY *= -1;
+			ball.xSpeed *= -1;
 		}
 		// Bottom screen collision
 		if (ball.y > GetScreenHeight())
 		{
 			ball.y = GetScreenHeight();
-			ball.speedY *= -1;
+			ball.ySpeed *= -1;
 		}
 
 		// Move left paddle up
 		if (IsKeyDown(KEY_W))
 		{
-			leftPaddle.y -= leftPaddle.speed * GetFrameTime();
+			leftPaddle.y -= leftPaddle.ySpeed * GetFrameTime();
 		}
 		// Move left paddle down
 		if (IsKeyDown(KEY_S))
 		{
-			leftPaddle.y += leftPaddle.speed * GetFrameTime();
+			leftPaddle.y += leftPaddle.ySpeed * GetFrameTime();
 		}
 		// Swing the paddle
 		if (IsKeyPressed(KEY_D))
 		{
-			leftPaddle.swingVel += 20;
+			leftPaddle.xSpeed += 20;
 		}
 		// Hold paddle back
 		if (IsKeyDown(KEY_A))
 		{
-			leftPaddle.swingVel = -10;
+			leftPaddle.xSpeed = -10;
 		}
 
 		// Move right paddle up
 		if (IsKeyDown(KEY_UP))
 		{
-			rightPaddle.y -= rightPaddle.speed * GetFrameTime();
+			rightPaddle.y -= rightPaddle.ySpeed * GetFrameTime();
 		}
 		// Move right paddle down
 		if (IsKeyDown(KEY_DOWN))
 		{
-			rightPaddle.y += rightPaddle.speed * GetFrameTime();
+			rightPaddle.y += rightPaddle.ySpeed * GetFrameTime();
 		}
 		// Swing right paddle
 		if (IsKeyPressed(KEY_LEFT))
 		{
-			rightPaddle.swingVel -= 20;
+			rightPaddle.xSpeed -= 20;
 		}
 		// Hold paddle back
 		if (IsKeyDown(KEY_RIGHT))
 		{
-			rightPaddle.swingVel = 10;
+			rightPaddle.xSpeed = 10;
 		}
 
 		// Left paddle and ball collision 
 		if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, 
-			leftPaddle.GetRect()) && ball.speedX < 0)
+			leftPaddle.GetRect()) && ball.xSpeed < 0)
 		{
-			std::cout << ball.speedX << "\n";
-			ball.speedX *= -1;
-			ball.speedX += leftPaddle.swingVel;
-			swingForce = leftPaddle.swingVel;
-			//ball.speedX += fabsf(ball.speedX) / ball.speedX * 10.0f;
+			std::cout << ball.xSpeed << "\n";
+			ball.xSpeed *= -1;
+			ball.xSpeed += leftPaddle.xSpeed;
+			swingForce = leftPaddle.xSpeed;
+			//ball.xSpeed += fabsf(ball.xSpeed) / ball.xSpeed * 10.0f;
 		}
 		// Right paddle and ball collision
 		if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius,
-			rightPaddle.GetRect()) && ball.speedX > 0)
+			rightPaddle.GetRect()) && ball.xSpeed > 0)
 		{
-			std::cout << ball.speedX << "\n";
-			ball.speedX *= -1;
-			ball.speedX += rightPaddle.swingVel;
-			swingForce = rightPaddle.swingVel;
-			//ball.speedX += fabsf(ball.speedX) / ball.speedX * 10.0f;
+			std::cout << ball.xSpeed << "\n";
+			ball.xSpeed *= -1;
+			ball.xSpeed += rightPaddle.xSpeed;
+			swingForce = rightPaddle.xSpeed;
+			//ball.xSpeed += fabsf(ball.xSpeed) / ball.xSpeed * 10.0f;
 		}
 
 		BeginDrawing(); // Begin drawing the window
@@ -189,7 +166,7 @@ int main()
 			rightPaddle.Draw(); // Render the right paddle
 			rightPaddle.BringBack(); // Oscilate the right padle
 			// Displays the speed of the ball
-			DrawText(TextFormat("Ball Speed: %f", abs(ball.speedX)), 100, 10, 20, BLUE);
+			DrawText(TextFormat("Ball Speed: %f", abs(ball.xSpeed)), 100, 10, 20, BLUE);
 			// Displays the speed of the last paddle to hit the ball on contact
 			DrawText(TextFormat("Swing Force: %f", swingForce), 400, 10, 20, BLUE);
 			DrawFPS(10, 10); // Displays the fps
